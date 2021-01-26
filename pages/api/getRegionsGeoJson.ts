1f1dbb4fc97dcff159326d395bb0b43cef03da59
Giuliano Mozzillo <mozzillation@me.com>
2021-01-26 18:13:52 +0100

    Update getRegionsGeoJson.ts

--- pages/api/getRegionsGeoJson.ts
+++ pages/api/getRegionsGeoJson.ts
@@ -1,6 +1,6 @@
 
 const getCurrentRegionGeoJson = async () => {
-	const res = await fetch('https://github.com/mozzillation/zone-covid/raw/main/data/regions.geojson')
+	const res = await fetch('https://github.com/mozzillation/zone-covid/blob/main/data/dataset.json?raw=true')
 	const regions = await res.json()
 
 	return regions
