03570c57bf46f4fad52ac0976f67d2d0bb87e03c
Giuliano Mozzillo <mozzillation@me.com>
2021-01-26 19:48:54 +0100

    Update const.ts

--- utils/const.ts
+++ utils/const.ts
@@ -11,20 +11,13 @@ export const GLOBAL_PAGE_VARIANT = {
 		transition: {
 			duration: 0.5,
 			ease: Power4.easeInOut,
-			delay: 0.5,
-			staggerChildren: 0.1,
-			staggerDirection: 1,
-			when: 'beforeChildren',
-			delayChildren: 10
+			delay: 0.5
 		}
 	},
 	exit: {
 		opacity: 0,
 		y: -50,
 		transition: {
-			staggerChildren: 1,
-			staggerDirection: -1,
-			when: 'afterChildren',
 			duration: 0.5,
 			ease: Power4.easeInOut
 		}
