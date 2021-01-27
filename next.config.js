8667d6b8c6bef2cd1e4a1b9650c681d0bd2c543f
Giuliano Mozzillo <mozzillation@me.com>
2021-01-27 16:43:26 +0100

    :rocket: Sitemap generator

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ const withPWA = require('next-pwa')
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
-  webpack: function(config) {
+  webpack:  (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/generate-sitemap.js');
+    }
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
@@ -14,3 +17,11 @@ module.exports = withPWA({
     dest: 'public'
   }
 })
+
+
+
+webpack: (config, { isServer }) => {
+
+
+  return config;
+}
---
+++ utils/generate-sitemap.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+
+const globby = require('globby')
+const prettier = require('prettier')
+
+const path = require('path')
+const process = require('process')
+const yaml = require('js-yaml');
+
+
+(async () => {
+	const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
+
+	const pathName = path.join(process.cwd(), 'data/content.yaml')
+	const { regions } = yaml.load(fs.readFileSync(pathName, 'utf8')).it
+
+	const regionPaths = Object.keys(regions).map(el => el.replace(/^/, '/'))
+
+	const staticPages = await globby([
+		'pages/**/*{.js,.mdx,.tsx}',
+		'!pages/_*.tsx',
+		'!pages/api',
+		'!pages/region',
+	])
+
+	const pages = [...regionPaths, ...staticPages]
+
+	console.log(pages)
+
+
+	const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages
+			.map(page => {
+				const path = page
+					.replace('index', '')
+					.replace('pages', '')
+					.replace('.js', '')
+					.replace('.tsx', '')
+				const route = path === '/index' ? '' : path
+
+				return `
+            <url>
+                <loc>${`https://checolo.re${route}`}</loc>
+            </url>
+                    `
+			})
+			.join('')}
+        </urlset>
+    `
+
+	// If you're not using Prettier, you can remove this.
+	const formatted = prettier.format(sitemap, {
+		...prettierConfig,
+		parser: 'html'
+	})
+
+	fs.writeFileSync('public/sitemap.xml', formatted)
+})()
