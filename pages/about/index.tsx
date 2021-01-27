98aad0ba4c03144d02b73ab7a3b8bc17432f3eea
Luca Milan <milan.luca@gmail.coim>
2021-01-27 22:04:01 +0100

    Add about content

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,8 +3,21 @@ import React from 'react'
 const About: React.FC = () => {
 
 	return (
-		<>
-		</>
+		<div>
+			<p>
+				CheColore ti aiuta a conoscere e rispettare le misure adottate dal Governo 
+				per fronteggiare l'emergenza epidemiologica da COVID-19.
+			</p>
+			<p>
+				Ciascun colore prevede precise disposizioni, che potrebbero però cambiare 
+				in base a ordinanze o decreti. Per infomazioni ufficiali, consulta sempre 
+				le <a href="http://www.governo.it/it/articolo/domande-frequenti-sulle-misure-adottate-dal-governo/15638">FAQ del Governo.</a> 
+			</p>
+			<p className="authors">
+				CheColore è un’idea di Edoardo Guido, Luca Milan, Giuliano Mozzillo e Jacopo Pompilii.
+			</p>
+
+		</div>
 	)
 }
 
