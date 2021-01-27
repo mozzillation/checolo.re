ae7c9047cc7ac888017bc55b177cd21610d4e723
Edoardo Guido <edoardo.guido.93@gmail.com>
2021-01-27 14:47:52 +0100

    👌 Improve Pre-Footer, add link to region's website

--- pages/region/[id].tsx
+++ pages/region/[id].tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { motion } from 'framer-motion'
-import { CaretDoubleDown } from 'phosphor-react'
+import { ArrowLineUpRight, CaretDoubleDown, ChatDots, IconProps } from 'phosphor-react'
 
 import { GlobalContext } from '@component/GlobalContext'
 import { DatePicker } from '@component/DatePicker'
@@ -35,7 +35,6 @@ const SingleRegion = ({ region, content, rules, data }: AppProps): JSX.Element =
 
 	}, [])
 
-
 	return (
 		<motion.div>
 			<div
@@ -56,11 +55,35 @@ const SingleRegion = ({ region, content, rules, data }: AppProps): JSX.Element =
 				<ActivityList zoneProps={zoneProps} rules={rules} />
 			</div>
 
+			<Disclaimer weight={'bold'} href={content.website} />
 			<Footer />
 		</motion.div>
 	)
 }
 
+const Disclaimer = ({ weight = 'regular', href }: { weight?: IconProps['weight'], href?: string }) => {
+
+	return (
+		<a href={href} target="_blank">
+			<div className={styles.disclaimer}>
+				<div className={styles.disclaimerIcon}>
+					{/* <ChatDots size={32} weight={weight} mirrored={true} /> */}
+				</div>
+				<div className={styles.disclaimerText} >
+					<div className={styles.primaryLabel}>
+						Hai bisogno di altre info?
+					</div>
+					<div className={styles.secondaryLabel}>
+						Clicca qui per consultare il sito della tua regione
+					</div>
+				</div>
+				<div className={styles.disclaimerIcon}>
+					<ArrowLineUpRight size={32} weight={weight} />
+				</div>
+			</div>
+		</a>
+	)
+}
 
 const ActivityList = ({ zoneProps, rules }: { zoneProps: any, rules: any }) => {
 
@@ -68,7 +91,7 @@ const ActivityList = ({ zoneProps, rules }: { zoneProps: any, rules: any }) => {
 
 	return (
 		<>
-			{currentRules ?.map((rule, index) => (
+			{currentRules?.map((rule, index) => (
 				<ActivityCard rule={rule} key={index} />
 			))}
 
@@ -82,9 +105,9 @@ const ActivityCard = ({ rule }) => {
 
 	return (
 		<div className={styles.activityCard}>
-			<span className={styles.emoji}>{rule ?.emoji}</span>
-			<span className={styles.title}>{rule ?.title}</span>
-			<span className={styles.subtitle}>{rule ?.subtitle}</span>
+			<span className={styles.emoji}>{rule?.emoji}</span>
+			<span className={styles.title}>{rule?.title}</span>
+			<span className={styles.subtitle}>{rule?.subtitle}</span>
 		</div>
 	)
 }
@@ -136,6 +159,8 @@ import fs from 'fs'
 import path from 'path'
 import process from 'process'
 import * as yaml from 'js-yaml'
+import Link, { LinkProps } from 'next/link'
+import { Url } from 'url'
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 	const id: string = params.id.toString()
