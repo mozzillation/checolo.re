030bef6b42908d1f194da9354d6995d4a3c7c266
Edoardo Guido <edoardo.guido.93@gmail.com>
2021-01-28 23:27:09 +0100

    👌 Cache geojson for future loads (no need to always fetch)

--- pages/api/getRegionsGeoJson.ts
+++ pages/api/getRegionsGeoJson.ts
@@ -1,11 +1,28 @@
-const getCurrentRegionGeoJson = async () => {
-	let address
-
-	address = 'https://raw.githubusercontent.com/mozzillation/checolo.re/main/data/regions.geojson'
+const fetchGeoJSON = async () => {
+	const address = 'https://raw.githubusercontent.com/mozzillation/checolo.re/main/data/regions.geojson'
 	const res = await fetch(address)
 	const regions = await res.json()
 
 	return regions
+}
+
+const getCurrentRegionGeoJson = async () => {
+
+	if ('localStorage' in window) {
+		const localStorageCached = window.localStorage.getItem('geojson')
+
+		if (!localStorageCached) {
+			const regions = fetchGeoJSON()
+			window.localStorage.setItem('geojson', JSON.stringify(regions))
+			return regions
+		} else {
+			return JSON.parse(localStorageCached)
+		}
+
+	} else {
+		return fetchGeoJSON()
+	}
+
 }
 
 export default getCurrentRegionGeoJson
