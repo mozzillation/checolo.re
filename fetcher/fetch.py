1cb53b0b9f1273f71da9502a8cccc1b607ab36c2
Edoardo Guido <edoardo.guido.93@gmail.com>
2021-04-07 20:59:32 +0200

    Revert old fetching logic
    Following dataset fix from Protezione Civile

--- fetcher/fetch.py
+++ fetcher/fetch.py
@@ -2,8 +2,8 @@ import os
 import zipfile
 import urllib.request
 
-zip_names = ['parte1.zip', 'parte2.z01']
 
+url = 'https://github.com/pcm-dpc/COVID-19/blob/master/aree/geojson/dpc-covid-19-aree-nuove-g-json.zip?raw=true'
 archive_name = 'file.zip'
 file_name = 'dpc-covid-19-ita-aree-nuove-g.json'
 
@@ -11,9 +11,7 @@ file_name = 'dpc-covid-19-ita-aree-nuove-g.json'
 def run(dir_name):
     zip_path = dir_name + archive_name
 
-    for zip in zip_names:
-        url = 'https://github.com/pcm-dpc/COVID-19/blob/master/aree/geojson/dpc-covid-19-aree-nuove-g-json-' + zip + '?raw=true'
-        urllib.request.urlretrieve(url, zip_path)
+    urllib.request.urlretrieve(url, zip_path)
 
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(dir_name)
